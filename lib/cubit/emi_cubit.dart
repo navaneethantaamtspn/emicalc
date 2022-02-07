600ed9475b0b4396a40e2fb97b595ff28c6e2f2e
navaneethantaamtspn <navaneethant@aamtspn.com>
2022-02-07 16:21:14 +0530

    make This Change Sai - Navaneethan

--- lib/cubit/emi_cubit.dart
+++ lib/cubit/emi_cubit.dart
@@ -7,16 +7,17 @@ part 'emi_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
 
-  void increment({required String a,required String b,required String c,required String d}) {
-
+  void increment(
+      {required String a,
+      required String b,
+      required String c,
+      required String d}) {
     double A = 0.0;
     int P = int.parse(a);
     double r = int.parse(b) / 12 / 100;
-    int n = d == "Year(s)" ? int.parse(c) * 12  : int.parse(c);
-    A = (P * r * pow((1+r), n) / ( pow((1+r),n) -1));
-
+    int n = d == "Year(s)" ? int.parse(c) * 12 : int.parse(c);
+    A = (P * r * pow((1 + r), n) / (pow((1 + r), n) - 1));
+//TODO: Limit the Result with two Decimal Places
     return emit(CounterState(counterValue: A));
   }
-
-
 }
